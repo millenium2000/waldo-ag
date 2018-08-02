3e431603b4dcc18bb710e715dc59826701037427
Alexandre Griniuk <alexandre_griniuk@epam.com>
2018-08-02 23:41:02 +0300

    refactoring a bit

--- subimage/src/main/subimage.py
+++ subimage/src/main/subimage.py
@@ -5,17 +5,21 @@ import math
 import numpy as np
 import numpy.linalg as linal
 
-#default threshold (max = 255*sqrt(3) )
-threshold_percent = 0.015
-threshold_percent = 0.05
-threshold = math.ceil(255*math.sqrt(3)*threshold_percent) #6
 
-_asdf = 10
-
-
-def savejpeg(fname, img, quality=95):
+def jpegSave(fname, img, quality=95):
 	cv2.imwrite(fname, img, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
 	return
+
+
+def jpegLoad(fname):
+	#read into ndarray with shape (Y, X, 3) and convert dtype to int16
+	# the last dimension is not RGB but BGR order
+	img = cv2.imread(fname)
+	if img is None:
+		raise Exception("could not read '{}' file".format(fname))
+	#convert into signed integers
+	img = np.int16(img)
+	return img
 
 
 # p : (y, x)
@@ -32,7 +36,29 @@ def computePoint0Delta(p:(int,int), img1, img2):
 	return linal.norm(img1[p] - img2[0,0])
 
 
-def findMatch(img1, img2):
+def drawRectangle(p, img, rect):
+	yFrom,xFrom = p
+	yTo = yFrom+rect[0]
+	xTo = xFrom+rect[1]
+	img[yFrom,xFrom:xTo,2] = 255
+	img[yTo,xFrom:xTo,2] = 255
+	img[yFrom:yTo,xFrom,2] = 255
+	img[yFrom:yTo,xTo,2] = 255
+	return
+
+
+def run(fname1, fname2, similarity=0.95, fnameOut=None):
+	#convert from similarity in % into max threshold in RGB distance
+	if similarity<0.0 or similarity>1.0:
+		raise Exception("invalid similarity provided '{}' must be between 0.0 and 1.0".format(similarity))
+	threshold = math.ceil(255.0*math.sqrt(3)*(1.0-similarity))
+	
+	#load files into (Y, X, 3) arrays
+	img1 = jpegLoad(fname1)
+	img2 = jpegLoad(fname2)
+	
+	#TODO: find the biggest image (if exists) and put it into img1
+	
 	#how many comparisons along Y and X axes
 	DY = img1.shape[0]-img2.shape[0]+1
 	DX = img1.shape[1]-img2.shape[1]+1
@@ -42,45 +68,23 @@ def findMatch(img1, img2):
 		if delta0 <= threshold:
 			deltaMax = computeMaxDelta(p, img1, img2)
 			if deltaMax <= threshold:
-				return p+(deltaMax,)
-	
+				if fnameOut is not None:
+					drawRectangle(p, img1, img2.shape)
+					jpegSave(fnameOut, img1, 100)
+				similar = 1.0 - deltaMax / (255.0*math.sqrt(3))
+				return p+(similar, deltaMax)
 	return None
-	
-
-def drawRectangle(p, img1, img2):
-	yFrom,xFrom = p
-	yTo = yFrom+img2.shape[0]
-	xTo = xFrom+img2.shape[1]
-	img1[yFrom,xFrom:xTo,2] = 255
-	img1[yTo,xFrom:xTo,2] = 255
-	img1[yFrom:yTo,xFrom,2] = 255
-	img1[yFrom:yTo,xTo,2] = 255
-	savejpeg("../target/tmp.jpg", img1, 100)
-	return
-
 
 def main(f1, f2):
-	#at this point the caller must ensure that sizes f1 >= f2
-	
-	#read into ndarray with shape (Y, X, 3) and convert dtype to int16
-	#note: the last dimension is not RGB but BGR order
-	img1 = np.int16(cv2.imread(f1))
-	img2 = np.int16(cv2.imread(f2))
-	
-	p = findMatch(img1, img2)
-	
-	if p is None:
-		print("not found")
-	else:
-		print("found at (x,y) = ("+str(p[1])+","+str(p[0])+")")
-		print("max delta: "+str(p[2]))
-		drawRectangle(p[:2], img1, img2)
-		#drawRectangle((147,412), img1, img2)
-	return
+	res = run(f1, f2, 0.96, "../target/out.jpg")
+	if res is None:
+		return 1
+	print("found {:0.4f} similarity at (x,y) = ({},{})".format(res[2], res[1], res[0]))
+	return 0
 
 
 
 if __name__ == "__main__":
-	main(sys.argv[1], sys.argv[2])
-	print("done")
+	res = main(sys.argv[1], sys.argv[2])
+	sys.exit(res)
 
